ef2ab0b37702242ae3c1471765576c803ce442df
ryhensel1994 <ryhensel@gmail.com>
2024-11-30 17:20:26 -0700

    updates

--- Graphviz/lib/graphviz/tcl/pkgIndex.tcl
+++
@@ -1,7 +0,0 @@
-package ifneeded Tcldot 12.2.0 "
-	load [file join $dir libtcldot.dll] Tcldot"
-package ifneeded Tclpathplan 12.2.0 "
-	load [file join $dir libtclplan.dll] Tclpathplan"
-package ifneeded Gdtclft 12.2.0 "
-	load [file join $dir libgdtclft.dll] Gdtclft"
-# end
